71808d43b1c05fc4e681a228c235aac50d93b0da
+ <+>
2024-04-04 11:34:51 +0300

    creates lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button'); // Replace 'button' with the ID of your element
+  
+    function clickAlert() {
+      alert('Button clicked!');
+    }
+  
+    button.addEventListener('click', clickAlert);
+  }
